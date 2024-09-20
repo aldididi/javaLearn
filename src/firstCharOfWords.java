f543bb6244b5cb6a0140cbbcb1ca9bbfe88a20e4
aldididi <aldyh024@gmail.com>
2024-09-20 22:35:15 +0700

    add orint each chars

--- src/firstCharOfWords.java
+++ src/firstCharOfWords.java
@@ -5,10 +5,11 @@ public class firstCharOfWords {
         Scanner s = new Scanner(System.in);
         String input = s.nextLine();
         firstCharOfWords1(input);
+       // printEachChar(input);
 
     }
 
-    public static void  firstCharOfWords1(String s) {
+    public static void firstCharOfWords1(String s) {
 
         String[] arr = s.split(" ");
         int length = arr.length;
@@ -19,21 +20,28 @@ public class firstCharOfWords {
 
             System.out.println(hasil);
         }
+    }
 
-//        public static void  lastCharOnWords(String s) {
-//
-//            String[] arr = s.split(" ");
-//            int length = arr.length;
-//
-//            for (int i = 0; i < length; i++) {
-//                String muksal = arr[i];
-//                char hasil = muksal.charAt(muksal.length()-1);
-//
-//                System.out.println(hasil);
-//            }
+    public static void lastCharOnWords(String s) {
+
+        String[] arr = s.split(" ");
+        int length = arr.length;
+
+        for (int i = 0; i < length; i++) {
+            String muksal = arr[i];
+            char hasil = muksal.charAt(muksal.length() - 1);
+            System.out.println(hasil);
+        }
+    }
 
 
-
+    public static void printEachChar(String s) {
+       for (int i = 0; i < s.length(); i++) {
+           System.out.println(s.charAt(i));
+       }
     }
 }
+
+
+
 
