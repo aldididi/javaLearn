171ac07e5c0ef2e677134e2db979fed758c6f1e1
aldididi <aldyh024@gmail.com>
2024-09-20 16:28:38 +0700

    add code firstChar of words

---
+++ src/firstCharOfWords.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class firstCharOfWords {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        firstCharOfWords1(input);
+
+    }
+
+    public static void  firstCharOfWords1(String s) {
+
+        String[] arr = s.split(" ");
+        int length = arr.length;
+
+        for (int i = 0; i < length; i++) {
+            String muksal = arr[i];
+            char hasil = muksal.charAt(0);
+
+            System.out.println(hasil);
+        }
+
+//        public static void  lastCharOnWords(String s) {
+//
+//            String[] arr = s.split(" ");
+//            int length = arr.length;
+//
+//            for (int i = 0; i < length; i++) {
+//                String muksal = arr[i];
+//                char hasil = muksal.charAt(muksal.length()-1);
+//
+//                System.out.println(hasil);
+//            }
+
+
+
+    }
+}
+
